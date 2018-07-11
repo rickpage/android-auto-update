822aad584e25e4751faf2b805c40c2df486a4fef
Page <rickpagersc@gmail.com>
2018-07-11 16:20:38 -0400

    url update

--- library/src/main/java/com/loveplusplus/update/Constants.java
+++ library/src/main/java/com/loveplusplus/update/Constants.java
@@ -16,5 +16,5 @@ class Constants {
 
     static final String TAG = "UpdateChecker";
 
-    static final String UPDATE_URL = "https://raw.githubusercontent.com/feicien/android-auto-update/develop/extras/update.json";
+    static final String UPDATE_URL = "https://raw.githubusercontent.com/rickpage/android-auto-update/develop/extras/update.json";
 }
